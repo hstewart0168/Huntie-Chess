b8fd16ab769555d340faeda70a6dfcadf2b785c8
Hunter Stewart <hstewart0168@myduneland.org>
2020-05-30 14:18:29 -0500

    FIRST STEPS
    Basics of JavaFX have been learned

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,29 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public void start(Stage stage) throws Exception{
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Huntie Chess");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        Text text = new Text(50,150,"Chess to Be Added in Future Update");
+        Group root = new Group(text);
+
+        Scene scene = new Scene(root, 300, 275);
+        scene.setFill(Color.BEIGE);
+
+        stage.setTitle("Huntie Chess");
+        stage.setScene(scene);
+        stage.show();
     }
 
 
